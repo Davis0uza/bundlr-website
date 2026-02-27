43b090071b2340b14fcfdcae0601d86497ebdd6f
Davi Souza <davitelmch@gmail.com>
2026-02-27 15:33:03 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,53 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.google-analytics.com https://js.stripe.com https://www.googletagmanager.com;
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+    img-src 'self' blob: data: https:;
+    font-src 'self' data: https://fonts.gstatic.com;
+    connect-src 'self' https://www.google-analytics.com https://api.stripe.com https://stats.g.doubleclick.net;
+    frame-src 'self' https://js.stripe.com https://www.google.com;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`.replace(/\n/g, '');
+
 const nextConfig: NextConfig = {
   /* config options here */
   async headers() {
     return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.trim(),
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+          }
+        ]
+      },
       {
         source: "/Icons/:path*",
         headers: [
